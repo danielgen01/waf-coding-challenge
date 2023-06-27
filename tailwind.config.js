faa5c10bf6016d05944bac954da417d03063085d
Daniel <gendik38@gmail.com>
2023-06-27 14:29:24 +0200

    SETUP

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,18 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      colors:{
+        "light-white":"#F5FFFF",
+        "dark-red" :"#FB635D",
+        "bright-red":"#F47266",
+        "half-black":"#0000007A",
+        "half-gray":"#4E4E4EC4",
+        "light-blue":"#C5DCFC",
+        "main-text-color":"#292929",
+        "bright-blue":"#C4DBFB",
+        "very-light-red":"#F6EEEE",
+        "grayish-white":"#EBF3FF"
+      }
     },
   },
   plugins: [],
