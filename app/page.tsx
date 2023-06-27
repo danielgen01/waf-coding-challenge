02b17695aa895793971f5a0b11b77b717e9dd985
Daniel <gendik38@gmail.com>
2023-06-27 16:02:05 +0200

    extend the border radius of the pic  to 100px

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
 import Image from 'next/image'
 import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
 
 export default function App() {
   return (
    <>
    <Header />
+   <main>
+    <Hero />
+
+
+   </main>
+
+
+   <footer>
+
+
+   </footer>
    </>
   )
 }
