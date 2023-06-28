e06008f110038f592523cb05ead54b285f08a4b3
Daniel <gendik38@gmail.com>
2023-06-28 10:04:57 +0200

    create functions and states for cart toggle status

--- app/page.tsx
+++ app/page.tsx
@@ -1,23 +1,24 @@
-import Image from 'next/image'
-import Header from './components/Header/Header'
-import Hero from './components/Hero/Hero'
-import Products from './components/Products/Products'
+"use client"
+import Image from "next/image"
+import Header from "./components/Header/Header"
+import Hero from "./components/Hero/Hero"
+import Products from "./components/Products/Products"
+import Cart from "./components/Cart/Cart"
+import { useState } from "react"
 
 export default function App() {
-  return (
-   <>
-   <Header />
-   <main>
-    <Hero />
-    <Products />
+	const [isCartOpen, setIsCartOpen] = useState<boolean>(false)
 
-   </main>
+	return (
+		<>
+			<Header isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
+			<main>
+				<Hero />
+				<Products />
+				<Cart isCartOpen={isCartOpen} setIsCartOpen={setIsCartOpen} />
+			</main>
 
-
-   <footer>
-
-
-   </footer>
-   </>
-  )
+			<footer></footer>
+		</>
+	)
 }
