2ac3114d2d5a32b21a0e4a53743acb463071dbdd
Daniel <gendik38@gmail.com>
2023-06-28 09:37:32 +0200

    add scrollbar for items-infinite scroll if needed

--- app/components/Cart/Cart.tsx
+++ app/components/Cart/Cart.tsx
@@ -9,7 +9,7 @@ const Cart: React.FC = () => {
 			<aside className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  lg:top-1 lg:right-2 bg-white  w-96 rounded-lg h-[90%]">
 				<div className="cart-content py-10 px-5 w-full h-full ">
 					<h1 className="text-center text-2xl font-bold">Cart</h1>
-					<ul className="cart-items mt-5 grid grid-cols-1 gap-y-32">
+					<ul className="cart-items mt-5 grid grid-cols-1 overflow-y-scroll h-[95%]">
 						<li>
 							<ListItem />
 						</li>
@@ -19,6 +19,7 @@ const Cart: React.FC = () => {
 						<li>
 							<ListItem />
 						</li>
+
 					</ul>
 				</div>
 			</aside>
