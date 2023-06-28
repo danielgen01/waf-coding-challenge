136a71deb296e5bc81d4e7b8b8d3ff893ddaf408
Daniel <gendik38@gmail.com>
2023-06-28 16:13:55 +0200

    add plus/minus buttons

--- app/components/Cart/ListItem.tsx
+++ app/components/Cart/ListItem.tsx
@@ -1,42 +1,43 @@
 import React from "react"
 import Image from "next/image"
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai"
 
 type Book = {
-	title:string
-	price:number
-	id:any 
-	image:string
-	quantity:number
+	title: string
+	price: number
+	id: any
+	image: string
+	quantity: number
 }
 
 type ListItemProps = {
-	item:Book
+	item: Book
 }
 
-const ListItem: React.FC<ListItemProps> = ({item}) => {
+const ListItem: React.FC<ListItemProps> = ({ item }) => {
 	console.log(item)
 
 	return (
 		<section className="cart-item grid grid-cols-3 w-full h-[33%]">
 			<div className="image-title-price flex flex-col items-center gap-2">
-				<Image
-					src={item.image}
-					width={75}
-					height={25}
-					alt="product-img"
-				/>
+				<Image src={item.image} width={75} height={25} alt="product-img" />
 				<span className="font-bold text-[#393280] text-md text-center xl:text-xl">
-				 {item.title}
+					{item.title}
 				</span>
-				<p className="text-[#ED553B] font-bold text-lg xl:text-xl">€ {item.price}</p>
+				<p className="text-[#ED553B] font-bold text-lg xl:text-xl">
+					€ {item.price}
+				</p>
 			</div>
 
 			<div className="quantity flex items-center justify-center ">
+				<AiOutlineMinus className="cursor-pointer" />
 				<input
 					type="number"
 					value={item.quantity}
-					className="w-10 border-2 border-gray-400 rounded-md text-center outline-light-blue"
+					readOnly={true}
+					className="w-10  rounded-md text-center outline-light-blue cursor-default"
 				/>
+				<AiOutlinePlus className="cursor-pointer" />
 			</div>
 
 			<div className="remove flex items-center justify-center">
