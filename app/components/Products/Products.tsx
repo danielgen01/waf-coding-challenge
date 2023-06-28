04e0c4e2f2c2d6bf6989d0a67c27c8934707efa0
Daniel <gendik38@gmail.com>
2023-06-28 16:03:28 +0200

    hand over usestate props

--- app/components/Products/Products.tsx
+++ app/components/Products/Products.tsx
@@ -3,7 +3,12 @@ import Booklist from "../../../BookList.json"
 import Image from "next/image"
 import { BsHandbag } from "react-icons/bs"
 
-const Products: React.FC = () => {
+type CartProps = {
+	cartItems: CartItem[]
+	setCartItems: any
+  }
+
+const Products: React.FC<CartProps> = ({ cartItems, setCartItems }) => {
 	return (
 		<section
 			className="py-32 flex flex-col px-10 lg:px-20 gap-5 mt-10 "
@@ -22,6 +27,8 @@ const Products: React.FC = () => {
 						author={book.author}
 						price={book.price}
 						id={book.id}
+						cartItems={cartItems}
+						setCartItems={setCartItems}
 					/>
 				))}
 			</ul>
@@ -31,20 +38,47 @@ const Products: React.FC = () => {
 
 export default Products
 
-type Book = {
+
+
+type CartItem = {
 	id: any
 	title: string
 	author: string
 	price: number
 	image: string
-}
+  }
+  
+  type ProductProps = CartItem & CartProps
+  
+  
 
-const Product: React.FC<Book> = ({ title, image, author, price, id }) => {
+const Product: React.FC<ProductProps> = ({
+	title,
+	image,
+	author,
+	price,
+	id,
+	cartItems,
+	setCartItems,
+}) => {
+
+	function addToCart() {
+		const newItem = { id, title, author, price, image }
+		setCartItems([...cartItems, newItem])
+		console.log(cartItems,)
+	}
+
 	return (
-		<div className="h-96 rounded-md ">
-			<div className="product-container-content px-10 py-10 shadow-lg h-72 flex flex-col gap-5 items-center">
-				<Image src={image} alt="img" width={120} height={100} className="hover:scale-105 duration-200"/>
-				<button className="flex items-center gap-2 bg-dark-red text-white font-normal rounded-md px-2 hover:opacity-80 duration-200 md:text-md xl:text-2xl py-1">
+		<div className="h-96 rounded-md product-entire-container">
+			<div className="product-content px-10 py-10 shadow-lg h-72 flex flex-col gap-5 items-center">
+				<Image
+					src={image}
+					alt="img"
+					width={120}
+					height={100}
+					className="hover:scale-105 duration-200"
+				/>
+				<button className="flex items-center gap-2 bg-dark-red text-white font-normal rounded-md px-2 hover:opacity-80 duration-200 md:text-md xl:text-2xl py-1"onClick={addToCart}>
 					Add to bag <BsHandbag />
 				</button>
 			</div>
