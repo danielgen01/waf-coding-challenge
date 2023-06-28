7f5081d127b411ded707a4ead79846fbd49d3957
Daniel <gendik38@gmail.com>
2023-06-28 16:02:40 +0200

    add the dynamic properties of the listitem in cart

--- app/components/Cart/ListItem.tsx
+++ app/components/Cart/ListItem.tsx
@@ -1,20 +1,33 @@
 import React from "react"
 import Image from "next/image"
 
-const ListItem: React.FC = () => {
+type Book = {
+	title:string
+	price:number
+	id:any 
+	image:string
+}
+
+type ListItemProps = {
+	item:Book
+}
+
+const ListItem: React.FC<ListItemProps> = ({item}) => {
+	console.log(item)
+
 	return (
-		<section className="cart-item grid grid-cols-3 w-full h-20 w-full">
+		<section className="cart-item grid grid-cols-3 w-full h-[33%]">
 			<div className="image-title-price flex flex-col items-center gap-2">
 				<Image
-					src={"/images/feuerklech.jpg"}
+					src={item.image}
 					width={75}
 					height={25}
 					alt="product-img"
 				/>
 				<span className="font-bold text-[#393280] text-md text-center xl:text-xl">
-					Title
+				 {item.title}
 				</span>
-				<p className="text-[#ED553B] font-bold text-lg xl:text-xl">Price</p>
+				<p className="text-[#ED553B] font-bold text-lg xl:text-xl">€ {item.price}</p>
 			</div>
 
 			<div className="quantity flex items-center justify-center ">
