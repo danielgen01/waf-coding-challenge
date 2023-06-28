9a691af976a5cca3518a26902d80379a4eee9753
Daniel <gendik38@gmail.com>
2023-06-28 09:17:18 +0200

    create own component for transparent background

---
+++ app/components/Cart/TransparentBG.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TransparentBackground: React.FC = () => {
+	return (
+		<div className="bg-black/50 absolute h-screen w-screen top-0 left-0"></div>
+	)
+}
+
+export default TransparentBackground
