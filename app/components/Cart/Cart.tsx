0028072ea195df38ee95b7beb75f42a481756c27
Daniel <gendik38@gmail.com>
2023-06-28 17:43:07 +0200

    fixed responsive design issues with cart

--- app/components/Cart/Cart.tsx
+++ app/components/Cart/Cart.tsx
@@ -30,7 +30,8 @@ const Cart: React.FC<cartprops> = ({
 	return (
 		<section style={{ display: isCartOpen ? "block" : "none" }}>
 			<TransparentBackground toggleCart={toggleCart} />
-			<aside className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:-translate-x-0 lg:-translate-y-0  2xl:left-[75%] lg:top-1 lg:right-2 bg-white w-[85%] rounded-lg h-[80%]">
+			<aside className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:-translate-x-0 lg:-translate-y-0  
+			2xl:left-[70%] lg:top-1 md:ml-auto bg-white w-[85%] lg:w-[500px] rounded-lg h-[80%] lg:h-[90%]">
 				<div className="cart-content py-10 px-5 w-full h-full ">
 					<h1 className="text-center text-2xl font-bold">Cart</h1>
 					<h2>Total: € {calculateTotal()}</h2>
