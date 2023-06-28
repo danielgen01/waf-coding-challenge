7ae2813882f740afaad42f220180a9b656f155f5
Daniel <gendik38@gmail.com>
2023-06-28 17:45:03 +0200

    hide the cart container overflow on x-axis

--- app/components/Cart/Cart.tsx
+++ app/components/Cart/Cart.tsx
@@ -31,7 +31,7 @@ const Cart: React.FC<cartprops> = ({
 		<section style={{ display: isCartOpen ? "block" : "none" }}>
 			<TransparentBackground toggleCart={toggleCart} />
 			<aside className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 lg:-translate-x-0 lg:-translate-y-0  
-			2xl:left-[70%] lg:top-1 md:ml-auto bg-white w-[85%] lg:w-[500px] rounded-lg h-[80%] lg:h-[90%]">
+			2xl:left-[70%] lg:top-1 md:ml-auto bg-white w-[85%] lg:w-[500px] rounded-lg h-[80%] lg:h-[90%] overflow-x-hidden">
 				<div className="cart-content py-10 px-5 w-full h-full ">
 					<h1 className="text-center text-2xl font-bold">Cart</h1>
 					<h2>Total: € {calculateTotal()}</h2>
